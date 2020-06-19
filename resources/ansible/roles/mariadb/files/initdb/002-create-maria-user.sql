a4cfecb9291cd5dafc56a4cc2cc5157243f220ce
Varlog Err <varlog.err@gmail.com>
2020-06-19 16:46:01 +0300

    reconfigure mariadb server

--- resources/ansible/roles/mariadb/files/initdb/002-create-maria-user.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'maria'@'localhost' IDENTIFIED BY 'Secret123#';
-CREATE USER 'maria'@'%' IDENTIFIED BY 'Secret123#';
-
-GRANT ALL PRIVILEGES ON *.* TO 'maria'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'maria'@'%';
